fad96decc1307f9f39dae7b0c911e006b3a3fad9
lewinj <78559672+lewinj@users.noreply.github.com>
2021-08-19 13:20:24 -0400

    Update PendulumTest.py

--- PendulumTest.py
+++ PendulumTest.py
@@ -5,4 +5,4 @@ omc.sendExpression("cd()")
 omc.sendExpression("loadModel(Modelica)")
 omc.sendExpression("loadFile(getInstallationDirectoryPath() + \"/share/doc/omc/testmodels/BouncingBall.mo\")")
 omc.sendExpression("instantiateModel(BouncingBall)")
-
+omc.sendExpression("simulate(BouncingBall, outputFormat="csv", startTime=0, stopTime=4, numberOfIntervals=5)")
