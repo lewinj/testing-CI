bd03c844d545987be62c0572d8ee7547dc3125f3
lewinj <78559672+lewinj@users.noreply.github.com>
2021-08-19 13:28:14 -0400

    Update PendulumTest.py

--- PendulumTest.py
+++ PendulumTest.py
@@ -1,8 +1,19 @@
+
+#from OMPython import OMCSessionZMQ
+#omc = OMCSessionZMQ()
+#omc.sendExpression("getVersion()")
+#omc.sendExpression("cd()")
+#omc.sendExpression("loadModel(Modelica)")
+#omc.sendExpression("loadFile(getInstallationDirectoryPath() + \"/share/doc/omc/testmodels/BouncingBall.mo\")")
+#omc.sendExpression("instantiateModel(BouncingBall)")
+#omc.sendExpression("simulate(BouncingBall, outputFormat="csv", startTime=0, stopTime=4, numberOfIntervals=5)")
 from OMPython import OMCSessionZMQ
 omc = OMCSessionZMQ()
-omc.sendExpression("getVersion()")
-omc.sendExpression("cd()")
-omc.sendExpression("loadModel(Modelica)")
-omc.sendExpression("loadFile(getInstallationDirectoryPath() + \"/share/doc/omc/testmodels/BouncingBall.mo\")")
-omc.sendExpression("instantiateModel(BouncingBall)")
-omc.sendExpression("simulate(BouncingBall, outputFormat="csv", startTime=0, stopTime=4, numberOfIntervals=5)")
+cmds = [
+  'loadFile(getInstallationDirectoryPath() + "/share/doc/omc/testmodels/BouncingBall.mo")',
+  "simulate(BouncingBall)",
+  "plot(h)"
+  ]
+for cmd in cmds:
+  answer = omc.sendExpression(cmd)
+  print("\n{}:\n{}".format(cmd, answer))
